49a71749956a04d796b85fb56ee87e5ba1668c10
anjaneya kamat <anjaneya@yahoo-inc.com>
2015-09-18 11:36:17 -0700

    Initial Commit

---
+++ TinderClient/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TinderClient
+//
+//  Created by anjaneya kamat on 9/18/15.
+//  Copyright (c) 2015 anjaneya kamat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TinderClient/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TinderClient
+//
+//  Created by anjaneya kamat on 9/18/15.
+//  Copyright (c) 2015 anjaneya kamat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
